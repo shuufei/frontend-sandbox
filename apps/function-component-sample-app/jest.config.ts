bb7ddaeb8ea7c565060170c298ac3952b2f3e263
shuufei <digital.shuufei@gmail.com>
2022-05-07 17:14:04 +0900

    chore: migrate nx to 14

--- apps/function-component-sample-app/jest.config.ts
+++ apps/function-component-sample-app/jest.config.ts
@@ -1,10 +1,11 @@
 module.exports = {
   displayName: 'function-component-sample-app',
-  preset: '../../jest.preset.js',
+
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/function-component-sample-app',
+  preset: '../../jest.preset.ts',
 };
