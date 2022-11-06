9d9dcb18b0871061fcf891e584bab974dc7a8daa
shuufei <digital.shuufei@gmail.com>
2022-11-06 18:18:50 +0900

    supabase-sample: fix: 認証処理をsupabase helperを利用して行う

--- apps/supabase-auth-sample-20221103/pages/_app.tsx
+++ apps/supabase-auth-sample-20221103/pages/_app.tsx
@@ -1,16 +1,20 @@
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useState } from 'react';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   return (
     <>
       <Head>
         <title>Welcome to supabase-auth-sample-20221103!</title>
       </Head>
-      <main className="app">
+      <SessionContextProvider supabaseClient={supabaseClient}>
         <Component {...pageProps} />
-      </main>
+      </SessionContextProvider>
     </>
   );
 }
--- apps/supabase-auth-sample-20221103/pages/index.tsx
+++ apps/supabase-auth-sample-20221103/pages/index.tsx
@@ -3,23 +3,23 @@ import { useRouter } from 'next/router';
 
 export function Index() {
   const router = useRouter();
-  useEffect(() => {
-    const hash = window.location.hash;
-    console.log('---url hash: ', hash);
-    const shouldRedirect = hash.startsWith('#access_token=');
-    const redirect = async () => {
-      await fetch('http://localhost:4200/api/auth/set-cookie', {
-        method: 'POST',
-        body: JSON.stringify({ hash }),
-      });
-      router.push('/home');
-    };
-    if (shouldRedirect) {
-      redirect();
-      return;
-    }
-    router.push('/home');
-  }, [router]);
+  // useEffect(() => {
+  //   const hash = window.location.hash;
+  //   console.log('---url hash: ', hash);
+  //   const shouldRedirect = hash.startsWith('#access_token=');
+  //   const redirect = async () => {
+  //     await fetch('http://localhost:4200/api/auth/set-cookie', {
+  //       method: 'POST',
+  //       body: JSON.stringify({ hash }),
+  //     });
+  //     router.push('/home');
+  //   };
+  //   if (shouldRedirect) {
+  //     redirect();
+  //     return;
+  //   }
+  //   router.push('/home');
+  // }, [router]);
   return (
     <div>
       <h1>Redirect...</h1>
